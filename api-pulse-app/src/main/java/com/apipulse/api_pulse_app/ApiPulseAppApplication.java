93f999b0506ba45e27acedf6c9d8b69c82d7544b
jihye0623 <wisdxx@gmail.com>
2026-03-16 13:53:45 +0900

    back-end setting

---
+++ api-pulse-app/src/main/java/com/apipulse/api_pulse_app/ApiPulseAppApplication.java
@@ -0,0 +1,13 @@
+package com.apipulse.api_pulse_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPulseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPulseAppApplication.class, args);
+	}
+
+}
---
+++ api-pulse-app/src/test/java/com/apipulse/api_pulse_app/ApiPulseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apipulse.api_pulse_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPulseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
